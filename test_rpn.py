c874e6e4953132339f0dc8ce2b7859f0c6d09282
obearyan <obearyan@umich.edu>
2018-10-26 11:55:20 -0400

    Final Commit

---
+++ test_rpn.py
@@ -0,0 +1,23 @@
+import unittest
+import rpn
+
+class TestBasics(unittest.TestCase):
+	def test_add(self):
+		result = rpn.calculate("1 1 +")
+		self.assertEqual(2, result)
+	
+	def test_sub(self):
+		result = rpn.calculate("4 3 -")
+		self.assertEqual(1, result)
+	
+	def test_mul(self):
+		result = rpn.calculate("4 3 *")
+		self.assertEqual(12, result)
+
+	def test_div(self):
+		result = rpn.calculate("4 2 /")
+		self.assertEqual(2, result)
+
+	def test_chain(self):
+		result = rpn.calculate("1 1 + 2 *")
+		self.assertEqual(4, result)
